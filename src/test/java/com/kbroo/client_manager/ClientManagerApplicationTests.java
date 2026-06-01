b23201210b89fe56b3afff1da958f127016efb89
Kbroo <kbroomyk@gmail.com>
2026-06-01 12:16:27 +0300

    First commit with index.html

---
+++ src/test/java/com/kbroo/client_manager/ClientManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbroo.client_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
